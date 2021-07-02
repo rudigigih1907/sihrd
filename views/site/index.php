3dc3ba8602a746016d05cbb917944bb64af6b182
dziljalal1 <dziljalal1@gmail.com>
2021-07-02 16:46:07 +0700

    Perbaiki UI

--- views/site/index.php
+++ views/site/index.php
@@ -15,9 +15,7 @@ $isGuest = Yii::$app->user->isGuest;
         <?php if (!$isGuest) : ?>
 
 
-
-            <div class="row mb-4">
-                <?php
+            <?php
                 $user= Yii::$app->user;
                 if (
                         $user->can('super-admin') ||
@@ -25,28 +23,31 @@ $isGuest = Yii::$app->user->isGuest;
                         $user->can('staff') ||
                         $user->can('direksi')
                 ): ?>
-                    <?= InfoBox::widget([
-                        'infoBoxText' => 'Loading ...',
-                        'icon' => FAS::_ADDRESS_CARD,
-                        'url' => Url::to(['/struktur-organisasi/hitung-jumlah-record', 'type' => app\models\StrukturOrganisasi::TIPE_PERUSAHAAN]),
-                        'urlTo' => Url::to(['/struktur-organisasi/index'])
-                    ]) ?>
+                    <div class="row mb-4">
+                        <?= InfoBox::widget([
+                            'infoBoxText' => 'Loading ...',
+                            'icon' => FAS::_ADDRESS_CARD,
+                            'url' => Url::to(['/struktur-organisasi/hitung-jumlah-record', 'type' => app\models\StrukturOrganisasi::TIPE_PERUSAHAAN]),
+                            'urlTo' => Url::to(['/struktur-organisasi/index'])
+                        ]) ?>
 
-                    <?= InfoBox::widget([
-                        'infoBoxText' => 'Karyawan',
-                        'icon' => FAS::_USERS,
-                        'url' => Url::to(['/karyawan/hitung-jumlah-record', 'kriteria' => app\models\Karyawan::AKTIF]),
-                        'urlTo' => Url::to(['/karyawan/index'])
-                    ]) ?>
+                        <?= InfoBox::widget([
+                            'infoBoxText' => 'Karyawan',
+                            'icon' => FAS::_USERS,
+                            'url' => Url::to(['/karyawan/hitung-jumlah-record', 'kriteria' => app\models\Karyawan::AKTIF]),
+                            'urlTo' => Url::to(['/karyawan/index'])
+                        ]) ?>
+                    </div>
 
                 <?php else: ?>
-                    <div class="jumbotron mx-3 shadow">
+                    <div class="jumbotron shadow">
                         <h1>Selamat Datang!</h1>
-                        <p class="lead">Pastikan biodata Anda sudah sesuai, Akses pada menu <span class="font-weight-bold"> Data Pribadi >> Profile Anda </span></p>
+                        <p class="lead">Pastikan biodata Anda sudah sesuai, Akses pada menu <span
+                                class="font-weight-bold"> Data Pribadi >> Profile Anda </span></p>
 
                     </div>
                 <?php endif; ?>
-            </div>
+
 
             <div class="row">
                 <div class="col col-md-6 col-lg-4">
@@ -59,8 +60,8 @@ $isGuest = Yii::$app->user->isGuest;
 
         <?php else: ?>
 
-            <div class="jumbotron mx-3 shadow">
-                <h1>Welcome!</h1>
+            <div class="jumbotron shadow">
+                <h1>Selamat Datang!</h1>
                 <p class="lead">Kamu harus login dulu untuk masuk ke Sistem HRD.</p>
                 <p>
                     <?= \yii\helpers\Html::a(FAS::icon(FAS::_SIGN_IN_ALT) ." Login", ['site/login'], [
